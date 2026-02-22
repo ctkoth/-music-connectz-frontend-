254e5118ff0fb964a4cd475a1622c7497957af65
ctkoth <ctkoth@gmail.com>
2026-02-22 12:37:03 -0500

    Add real Stripe publishable key

--- stripe-config.js
+++ stripe-config.js
@@ -2,5 +2,5 @@
 // stripe-config.js
 // Only include your Stripe publishable key here (never your secret key)
 window.stripeConfig = {
-  publishableKey: "pk_test_REPLACE_WITH_YOUR_PUBLISHABLE_KEY"
+  publishableKey: "pk_test_51StwuJLQbJylC2pG37HAt1BdlNHnYcvUM8wEEHilhBPpt92ztXBe9Q8sY3CdNTWFGnCWWFvcIpSSXqYRvmZDKfs700iPeoqxip"
 };
