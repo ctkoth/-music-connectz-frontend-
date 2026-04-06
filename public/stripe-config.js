07cd3fed9554bd68224d35e7087301ae5f66857a
ctkoth <ctkoth@gmail.com>
2026-04-06 17:48:47 -0400

    Switch Stripe to live publishable key

--- public/stripe-config.js
+++ public/stripe-config.js
@@ -2,5 +2,5 @@
 // stripe-config.js
 // Only include your Stripe publishable key here (never your secret key)
 window.stripeConfig = {
-  publishableKey: "pk_test_51StwuJLQbJylC2pG37HAt1BdlNHnYcvUM8wEEHilhBPpt92ztXBe9Q8sY3CdNTWFGnCWWFvcIpSSXqYRvmZDKfs700iPeoqxip"
+  publishableKey: "pk_live_51StwuJLQbJylC2pGeL94LgbzsoDtEisfP0REcM3LIF32cV2pabbQihFMUXwj27dAhPEdXIr8NkXijpsh59zRgHU00rmJiTquA"
 };
